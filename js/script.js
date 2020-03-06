bd06c1e5af76692c07d8f7178c9ee50fd96b0708
Eimaytto <louis04060@gmail.com>
2020-03-06 18:18:27 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 window.onload = function(){
 	message.innerHTML = "";
-	var file = "https://raw.githubusercontent.com/Eimayto/forcontest/master/download/file"+i+".txt";
+	var file = "https://raw.githubusercontent.com/Eimayto/rootomato_language/master/Rootomato_Dictionary.txt";
 	var rawFile = new XMLHttpRequest();
 	rawFile.open("GET", file, false);
 	rawFile.onreadystatechange = function ()
 	{
-	  if(rawFile.readyState === 4)
-	  {
-      if(rawFile.status === 200 || rawFile.status == 0)
-      {
-        var allText = rawFile.responseText;
-        body = allText;
-      }
-      }
-	  }
-	  rawFile.send(null);
-  }
+		if(rawFile.readyState === 4)
+		{
+			if(rawFile.status === 200 || rawFile.status == 0)
+			{
+			var allText = rawFile.responseText;
+			}
+		}
+		rawFile.send(null);
+		
+	}
 }
