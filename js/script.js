dc1098930e69c9aed4f7c7a370b54a867cba1b25
Eimaytto <louis04060@gmail.com>
2020-03-06 18:55:12 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var dictonary = {};
+var dic = {};
 window.onload = function(){
 	var file = "https://raw.githubusercontent.com/Eimayto/rootomato_language/master/Rootomato_Dictionary.txt";
 	var rawFile = new XMLHttpRequest();
@@ -15,16 +15,18 @@ window.onload = function(){
 		rawFile.send(null);
 		var i = 0;
 		while(/([^:]+)([^/]+)/.test(allText)){
-			var word = /([^:]+)([^/]+)./.exec(body);
+			var word = /([^:]+)([^/]+)./.exec(allText);
 			allText = allText.replace(word[0],'');
-			dictionary[i][0] = word[1];
-			dictionary[i++][1] = word[2];
+			dic[i][0] = word[1];
+			dic[i++][1] = word[2];
 		}
 		var print = "<table border='1'>";
-		for(var i = 0; i < dictionary.length; i++){
-			print = print+"<tr><th>"+String(dictionary[i][0])+"</th><th>"+String(dictionary[i][1])+"</th></tr>";
+		for(var i = 0; i < dic.length; i++){
+			print = print+"<tr><th>"+String(dic[i][0])+"</th><th>"+String(dic[i][1])+"</th></tr>";
 		}
 		print = print+"</table>";
+		alert(dic);
+		alert(print);
 		document.getElementById('table').innerHTML = print;
 	}
 }
