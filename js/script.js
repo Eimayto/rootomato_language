9e87290c0f189813e93eab4067235f97d84c370d
Eimaytto <louis04060@gmail.com>
2020-03-06 19:14:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,11 @@
-var dic = {};
+var dic = [["캜","섭씨"],["팛","화씨"],["간딘단","간단"],["무료","무려"],["광대","관대"],["디ㅗㅋ","디코"],["잃",1],["읺",2],["삶",3],["삲",4],["옳",5],["윣",6],["칡",7],["팕",8],["굵",9],["싮",10],["밹",100],["춹",1000]];
 window.onload = function(){
-	var file = "https://raw.githubusercontent.com/Eimayto/rootomato_language/master/Rootomato_Dictionary.txt";
-	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", file, false);
-	rawFile.onreadystatechange = function ()
-	{
-		if(rawFile.readyState === 4)
-		{
-			if(rawFile.status === 200 || rawFile.status == 0)
-			{
-				var allText = rawFile.responseText;
-			}
-		}
-		rawFile.send(null);
-		var i = 0;
-		while(/([^:]+)([^/]+)/.test(allText)){
-			var word = /([^:]+)([^/]+)./.exec(allText);
-			allText = allText.replace(word[0],'');
-			dic[i][0] = word[1];
-			dic[i++][1] = word[2];
-		}
-		var print = "<table border='1'>";
-		for(var i = 0; i < dic.length; i++){
-			print = print+"<tr><th>"+String(dic[i][0])+"</th><th>"+String(dic[i][1])+"</th></tr>";
-		}
-		print = print+"</table>";
-		alert(dic);
-		alert(print);
-		document.getElementById('table').innerHTML = print;
+	var print = "<table border='1'>";
+	for(var i = 0; i < dic.length; i++){
+		print = print+"<tr><th>"+String(dic[i][0])+"</th><th>"+String(dic[i][1])+"</th></tr>";
 	}
+	print = print+"</table>";
+	alert(dic);
+	alert(print);
+	document.getElementById('table').innerHTML = print;
 }
