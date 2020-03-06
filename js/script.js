668ea93d4942d247138b2b7e8912ba953f6b354e
Eimaytto <louis04060@gmail.com>
2020-03-06 18:28:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var dictonary = {};
 window.onload = function(){
 	message.innerHTML = "";
 	var file = "https://raw.githubusercontent.com/Eimayto/rootomato_language/master/Rootomato_Dictionary.txt";
@@ -9,10 +10,16 @@ window.onload = function(){
 		{
 			if(rawFile.status === 200 || rawFile.status == 0)
 			{
-			var allText = rawFile.responseText;
+				var allText = rawFile.responseText;
 			}
 		}
 		rawFile.send(null);
-		
+		var i = 0;
+		while(/([^:]+)([^/]+)/.test(allText)){
+			var word = /([^:]+)([^/]+)./.exec(body);
+			allText = allText.replace(word[0],'');
+			dictionary[i][0] = word[1];
+			dictionary[i++][1] = word[2];
+		}
 	}
 }
