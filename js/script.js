3209babebe7870786e16887ab5a1cacb8828711a
Eimaytto <louis04060@gmail.com>
2020-03-06 18:40:08 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ window.onload = function(){
 			dictionary[i][0] = word[1];
 			dictionary[i++][1] = word[2];
 		}
-		var print = "<table border="1">";
+		var print = "<table border='1'>";
 		for(var i = 0; i < dictionary.length; i++){
 			print = print+"<tr><th>"+String(dictionary[i][0])+"</th><th>"+String(dictionary[i][1])+"</th></tr>";
 		}
